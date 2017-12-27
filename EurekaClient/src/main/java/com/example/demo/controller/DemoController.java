cf5bd9e9165dcd2c9c23b3c171b1e3e8b2a40f8d
Dbsahoo <debesh.tcs@gmail.com>
2017-12-27 14:32:51 -0600

    Changes for Spring boot zuul

---
+++ EurekaClient/src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	@RequestMapping("/getName")
+	public String getName(){
+		return "DemoProject";
+	}
+	
+}
